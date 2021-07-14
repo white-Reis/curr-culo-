09c6cbe95404fa614826abf4d3c69c877685594a
Fabio Reis <82837278+white-Reis@users.noreply.github.com>
2021-07-14 15:50:12 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+var nome = "Fabio Reis";
+var cargo = "Desenvolverdor Full Stack";
+
+var nomeHtml = document.getElementById("name");
+var cargoHtml = document.getElementById("cg");
+var texto1 = document.getElementById("texto-1");
+var texto2 = document.getElementById("texto-2");
+
+function colocarNomeNoHtml(nome) {
+    nomeHtml.innerHTML = nome;
+}
+
+function colocarCargoNoHtml(cargo) {
+    cargoHtml.innerHTML = cargo;
+}
+function logarNome() {
+    console.log(nome);
+}
+function ClickNoProjetos() {
+    console.log("clickou no botão projetos");
+    texto2.style.display= "none";
+    texto1.style.display= "block";
+}
+function ClickNoSobre() {
+    console.log("clickou no botão sobre");
+    texto2.style.display= "block";
+    texto1.style.display= "none";
+}
+
+colocarCargoNoHtml(cargo);
+colocarNomeNoHtml(nome);
+
